61650ff42861a8758b10728d8bd015d840c2642e
AntonioThone <antoniothone6@gmail.com>
2025-03-27 10:12:28 +0100

    heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import {Heading} from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App(){
     return(
         <>
-            <h1>this is where i start</h1>
+            <Heading/>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sequi pariatur facere eveniet reiciendis, dicta vitae quibusdam doloribus nostrum vero nisi officia adipisci quam facilis repudiandae perspiciatis provident quos laborum?
             </p>
         </>
