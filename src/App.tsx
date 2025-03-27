6503a3a9f69b87499e0b76b82c966211882d4df3
AntonioThone <antoniothone6@gmail.com>
2025-03-27 10:56:23 +0100

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ export function App(){
     return(
         <>
             <Heading/>
+            <Heading attr={123} attr2='string'>
+                ola mundo 1
+            </Heading>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sequi pariatur facere eveniet reiciendis, dicta vitae quibusdam doloribus nostrum vero nisi officia adipisci quam facilis repudiandae perspiciatis provident quos laborum?
             </p>
         </>
