3db2c7b04aacc8cb4c4a4bfd5c5dbd06595e7ce7
AntonioThone <antoniothone6@gmail.com>
2025-03-27 16:10:57 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import {Heading} from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
+import {  TimerIcon } from 'lucide-react';
 
 export function App(){
     return(
         <>
-            <Heading/>
-            <Heading attr={123} attr2='string'>
+            <Heading>
                 ola mundo 1
+
+                <button>
+                    <TimerIcon/>
+                </button>
             </Heading>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sequi pariatur facere eveniet reiciendis, dicta vitae quibusdam doloribus nostrum vero nisi officia adipisci quam facilis repudiandae perspiciatis provident quos laborum?
             </p>
