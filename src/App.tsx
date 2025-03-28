f1c8a3995f907dc5deb08e49c88174e1e56162c0
AntonioThone <antoniothone6@gmail.com>
2025-03-28 08:40:40 +0100

    container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import {Heading} from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
-import {  TimerIcon } from 'lucide-react';
 
 export function App(){
     return(
